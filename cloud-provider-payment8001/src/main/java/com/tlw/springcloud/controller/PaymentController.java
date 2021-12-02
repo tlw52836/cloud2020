01142809a210838e0f878d89bae0b4480c66071a
tlw52836 <1660872260@qq.com>
2021-12-02 15:10:28 +0800

    new moudle

--- cloud-provider-payment8001/src/main/java/com/tlw/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/tlw/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.tlw.springcloud.entities.CommonResult;
 import com.tlw.springcloud.entities.Payment;
 import com.tlw.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,6 @@ public class PaymentController {
     public CommonResult create(Payment payment) {
         int result = paymentService.create(payment);
         log.info("***插入结果：" + result);
-
         if (result > 0) {
             return new CommonResult(200, "插入数据成功", result);
         } else {
@@ -29,8 +29,8 @@ public class PaymentController {
         }
     }
 
-    @GetMapping(value = "/payment/getPaymentById")
-    public CommonResult getPaymentById(Long id) {
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("***查询结果：" + payment);
 
