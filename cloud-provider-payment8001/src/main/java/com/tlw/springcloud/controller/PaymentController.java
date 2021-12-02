d6873463ec6cf00b7b6b900685575cdc7e40db44
tlw52836 <1660872260@qq.com>
2021-12-02 15:54:46 +0800

    new moudle

--- cloud-provider-payment8001/src/main/java/com/tlw/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/tlw/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.tlw.springcloud.entities.Payment;
 import com.tlw.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment) {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("***插入结果：" + result);
         if (result > 0) {
