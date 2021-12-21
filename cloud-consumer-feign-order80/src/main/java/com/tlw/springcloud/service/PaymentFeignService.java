5df4c152c634c8bbcb9046e6b506e84f03b475d3
tlw52836 <1660872260@qq.com>
2021-12-21 15:32:14 +0800

    openfeign

--- cloud-consumer-feign-order80/src/main/java/com/tlw/springcloud/service/PaymentFeignService.java
+++ cloud-consumer-feign-order80/src/main/java/com/tlw/springcloud/service/PaymentFeignService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
 @FeignClient(value = "CLOUD-PAYMENT-SERVICE")
 public interface PaymentFeignService {
     @GetMapping(value = "/payment/get/{id}")
